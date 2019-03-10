10f3e969d48d1b98079cb0cabdde1bbc02ec30be
wontak <ryu071511@gmail.com>
2019-03-10 23:44:43 +0900

    update

--- geneticAlgorithm/GeneticAlgorithm.py
+++ geneticAlgorithm/GeneticAlgorithm.py
@@ -1,7 +1,6 @@
 """
 reference : https://towardsdatascience.com/evolution-of-a-salesman-a-complete-genetic-algorithm-tutorial-for-python-6fe5d2b3ca35
 """
-
 import numpy as np, random, operator, pandas as pd, matplotlib.pyplot as plt
 
 class city:
@@ -70,24 +69,112 @@ def rankRoutes(population):
     # value를 기준으로 정렬
     return sorted(fitnessResult.items(), key=operator.itemgetter(1), reverse=True)
 
+def selection(popRanked, eliteSize):
+    """
+     Fitness proportionate selection
+     - popRanked : rankRoutes
+     - eliteSize : select size
+     return selctionResults
+     """
 
-class GeneticAlgorithm:
+    selectionResults=[]
+    df=pd.DataFrame(np.array(popRanked),columns=["Index","Fitness"])
+    df['cum_sum']=df.Fitness.cumsum() # cumulative sum
+    df['cum_perc']=100*df.cum_sum/df.Fitness.sum()
 
-    def __init__(self):
-        return None
+    for i in range(0, eliteSize):
+        selectionResults.append(popRanked[i][0])
+    for i in range(0, len(popRanked)-eliteSize):
+        pick=100*random.random()
+        for i in range(0, len(popRanked)):
+            if pick<=df.iat[i,3]:
+                selectionResults.append(popRanked[i][0])
+                break
 
-    def perform(self):
-        return None
-
-    def selection(self):
-        return None
-
-    def mutation(self):
-        return None
+    return selectionResults
 
 
-    def crossover(self):
+def matingPool(population, selectionResults):
+    matingpool=[]
+    for i in range(0, len(selectionResults)):
+        index=selectionResults[i]
+        matingpool.append(population[index])
+    return matingpool
 
-        return None
-    def substitute(self):
-        return None
+
+def breed(parent1, parent2):
+    child=[]
+    childP1=[]
+    childP2=[]
+
+    geneA=int(random.random()*len(parent1))
+    geneB=int(random.random()*len(parent2))
+
+    startGene=min(geneA,geneB)
+    endGene=max(geneA,geneB)
+
+    for i in range(startGene,endGene):
+        childP1.append(parent1[i])
+
+    childP2=[item for item in parent2 if item not in childP1]
+
+    child=childP1+childP2
+
+    return child
+
+def breedPopulation(matingpool, eliteSize):
+    children=[]
+    length=len(matingpool)-eliteSize
+    pool=random.sample(matingpool, len(matingpool))
+
+    for i in range(0, eliteSize):
+        children.append(matingpool[i])
+
+    for i in range(0, length):
+        child=breed(pool[i], pool[len(matingpool)-i-1])
+        children.append(child)
+    return children
+
+
+def mutate(individual, mutationRate):
+    """ avoid local convergence"""
+    for swapped in range(len(individual)):
+        if (random.random() < mutationRate):
+            swapWith=int(random.random()*len(individual))
+
+            city1=individual[swapped]
+            city2=individual[swapped]
+
+            individual[swapped]=city2
+            individual[swapWith]=city1
+    return individual
+
+def mutatePopulation(population, mutationRate):
+    mutatePop=[]
+
+    for ind in range(0, len(population)):
+        mutatedInd=mutate(population[ind], mutationRate)
+        mutatePop.append(mutatedInd)
+    return mutatePop
+
+def nextGeneration(currentGen, eliteSize, mutationRate):
+    popRanked=rankRoutes(currentGen)
+    selectionResults=selection(popRanked,eliteSize)
+    matingpool=matingPool(currentGen, selectionResults)
+    children=breedPopulation(matingpool,eliteSize)
+    nextGeneration=mutatePopulation(children,mutationRate)
+    return nextGeneration
+
+
+def geneticAlgorithm(population,popSize, eliteSize, mutationRate, generations):
+    pop=initialPopulation(popSize,population)
+    print("Initial distnace :"+ str(1/rankRoutes(pop)[0][1]))
+
+    for i in range(generations)
+        pop=nextGeneration(pop,eliteSize,mutationRate)
+    print("Final distance : "+str(1/rankRoutes(pop)[0][1]))
+
+    bestRouteIndex=rankRoutes(pop)[0][0]
+    bestRoute=pop[bestRouteIndex]
+    return bestRoute
+
